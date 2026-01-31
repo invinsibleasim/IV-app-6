8baf57af3c82b298e8e5fe75a660f2beb329a77d
invinsibleasim <43987873+invinsibleasim@users.noreply.github.com>
2026-01-31 17:17:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
-import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+from damp_t.io_utils import load_light_iv, load_dark_iv, load_sunsvoc
+from damp_t.translate import damp_t_pipeline
+from damp_t.plotting import plot_iv_sets
+import matplotlib.pyplot as plt
+
+light = load_light_iv('examples/light_iv.csv')
+dark  = load_dark_iv('examples/dark_iv.csv')
+suns  = load_sunsvoc('examples/sunsvoc.csv')  # optional but recommended
+
+res = damp_t_pipeline(light, dark, suns, target_G=1000.0, target_T=25.0)
+fig = plot_iv_sets(res['anchor_curve'], res['translated'], res['neutralized'])
+plt.show()
+
